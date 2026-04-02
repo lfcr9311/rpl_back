523b0ff75d3ef9df79948e1795d3e664ddb62758
Luis Felipe Rossi <lfcr93@gmail.com>
2026-04-02 12:43:13 -0300

    fsdf

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,25 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://rpl-front.vercel.app',
+    'https://rpl-back.vercel.app',
+  ]
+
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://127.0.0.1:5173',
-      'https://rpl-back.vercel.app',
-    ],
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true)
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true)
+      }
+
+      return callback(new Error(`Origin ${origin} not allowed by CORS`), false)
+    },
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: false,
